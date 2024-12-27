1fa34bf9f7790d7347b670ac58a26624e4accb6e
T8 <108910965+T888R@users.noreply.github.com>
2024-12-27 11:55:37 -0700

    added starship zsh config

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Adding dotfiles bare repo for easy convinience
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias vim=nvim
---
+++ .config/zsh/exports.zsh
@@ -0,0 +1,11 @@
+#!/bin/sh
+HISTSIZE=1000000
+SAVEHIST=1000000
+export EDITOR="nvim"
+export TERMINAL="kitty"
+export BROWSER="brave-browser"
+
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+
+export PATH="$HOME/.local/bin":$PATH
+export PATH="$HOME/.cargo/bin":$PATH
